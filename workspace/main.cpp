515b4c97dda30482611e21331c435ec9c4e4a69d
mattofthekidd <mattofthekidd@gmail.com>
2017-01-12 02:24:25 +0000

    Updated readme with cmd line arguement to compile and run code in c9.

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -6,10 +6,10 @@
 int main()
 {
     Employee emp1("Jane Smith", 35000);
-   // Employee emp2("Bill Brown", 32000);
+    Employee emp2("Bill Brown", 32000);
     
     std::cout << emp1.toString() << std::endl;
-    //std::cout << emp2.toString() << std::endl;
+    std::cout << emp2.toString() << std::endl;
     
     
     return 0;
